2f7b004a92499eb03a706877b80905c5762b4872
unknown <SammeV@BIO-WRK-DFN4014.BIO.ad10.intern.kuleuven.ac.be>
2015-12-04 14:22:27 +0100

    Added function to retrieve general information about a project

---
+++ getProjectInfo.m
@@ -0,0 +1,27 @@
+function getProjectInfo(varargin)
+    % Gathers information about the project and prints it out
+    % input: filename, projectname or setuptable
+    
+    switch class(varargin{1})
+        case 'char'
+            if(exist(varargin{1},'file') == 2)
+                setuptable = load(varargin{1});
+                [folder,name,~] = fileparts(varargin{1});
+            else
+                files = openProject(varargin{1});
+                setuptable = files.setuptable;
+                name = varargin{1};
+                folder = ''; %TODO
+            end
+        case 'cell'
+            setuptable = varargin{1};
+            name = '';
+            folder = '';
+        otherwise
+            error('Incorrect input.');
+    end
+    
+    fprintf('==== Project Information ====\n');
+    fprintf('Name: %s\nFolder: %s\n\n',name,folder);
+    fprintf('No of slices: %d\nNo of conditions: %d\nNo of Mice: %d\n\n',size(setuptable,1),size(unique(setuptable(:,1)),1),size(unique(setuptable(:,2)),1));
+    fprintf('Midline registered: %d/%d\n\n',size(cellfun(@(x) isfield(x,'midlinep'),setuptable(:,5)),1), size(setuptable,1));
